ec09a45c4089986c5a7ef5e8ea574f0e6f9a03a3
nikita <nikita.bobers@gmail.com>
2021-02-14 18:50:12 +0200

    Routes created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Header } from './components/layout/Header';
+import { UserForm } from './components/routes/userForm/UserForm';
+import { UserList } from './components/routes/userList/UserList';
 
 function App() {
-    return <div className="App">App</div>;
+    return (
+        <Router>
+            <Fragment>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={UserList} />
+                    <Route exact path="/user" component={UserForm} />
+                </Switch>
+            </Fragment>
+        </Router>
+    );
 }
 
 export default App;
