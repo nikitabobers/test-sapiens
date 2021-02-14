9a069a906b1d72388c8991d00a0f240436579a65
nikita <nikita.bobers@gmail.com>
2021-02-15 01:03:01 +0200

    CORS middleware added

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express, { Application } from 'express';
+const cors = require('cors');
+
 import connectDB from './config/db';
 import router from './routes';
 
@@ -12,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
